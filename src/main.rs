ab931eb7ad3fbadf891eeeb974635595bde279e6
Arter Tendean <arter.tendean.07@gmail.com>
2025-06-11 00:28:43 +0700

    Add number conversion test and examples

--- src/main.rs
+++ src/main.rs
@@ -47,8 +47,36 @@ fn number(){
     // Default type is i32
     let a = 10;
     println!("a = {a}");
-    
+
     // Default type is f64
     let b = 3.14;
     println!("b = {b}");
+}
+
+#[test]
+fn number_conversion(){
+    // Number conversion
+    // i8, i16, i32, i64, i128
+
+    let a :i8 = 10;
+    println!("a = {a}");
+
+    let b :i16 = a as i16;
+    println!("b = {b}");
+
+    let c :i32 = b as i32;
+    println!("c = {c}");
+
+    let d :i64 = c as i64;
+    println!("d = {d}");
+
+    let e :i128 = a as i128;
+    println!("e = {e}");
+
+    // Overflow example
+    // let mut f :i16 = 32767;
+    // println!("f = {f}");
+    // f += 1; // This will cause an overflow in debug mode
+    // println!("f after overflow = {f}");
+
 }
